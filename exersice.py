633712e0bc44379acfb55fc31f20adbb24517533
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-09-20 19:08:29 +0500

    exersice.py

--- exersice.py
+++ exersice.py
@@ -1,1 +1,9 @@
+#     Exersice 1
 
+a = 50
+b = 3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
