62c0330b874537260101ea1141db9b1d41f40a36
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-09-20 19:03:15 +0500

    Practise code for the course

---
+++ practise.py
@@ -0,0 +1,260 @@
+#     Day 1
+
+print("Hello World")
+print(5)
+
+#     Day 3
+
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
+# Dont worry about how to use these modules just yet!
+pandas.read_csv("one.csv")
+m = hashlib.sha256()
+
+#     Day 4
+
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)
+
+#     Day 6
+
+a = complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
+
+
+#     Day 7
+
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
+
+#     Day 8
+
+a = 50
+b = 3
+
+print("The value of", a, "+", 3, "is: ", a + b)
+print("The value of", a, "-", 3, "is: ", a - b)
+print("The value of", a, "*", 3, "is: ", a * b)
+print("The value of", a, "/", 3, "is: ", a / b)
+
+#     Day 9
+
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+
+print(c + d)
+
+#     Day 10
+
+a = input("Enter your name: ")
+print("My name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x  + y)
+
+print(int(x) + int(y))
+
+#     Day 11
+
+name = "Harry"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+apple = '''He said, 
+Hi Harry
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+# print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # Throws an error
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
+
+#     Day 12 
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:4]) # including 0 but not 4
+print(fruit[1:4]) # including 1 but not 4
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[:len(fruit)-3])
+print(fruit[-1:len(fruit) - 3])
+print(fruit[-3:-1])
+
+#     Day 13
+
+# Strings are immutable
+a = "!!!Harry!! !!!!!!!!! Harry"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Harry", "John"))
+print(a.split(" "))
+blogHeading = "introduction tO jS"
+print(blogHeading.capitalize())
+
+str1 = "Welcome to the Console!!!"
+print(len(str1))
+print(len(str1.center(50)))
+print(a.count("Harry"))
+
+str1 = "Welcome to the Console !!!"
+print(str1.endswith("!!!"))
+
+str1 = "Welcome to the Console !!!"
+print(str1.endswith("to", 4, 10))
+
+str1 = "He's name is Dan. He is an honest man."
+print(str1.find("ishh"))
+print(str1.index("ishh"))
+
+str1 = "WelcomeToTheConsole"
+print(str1.isalnum())
+str1 = "Welcome"
+print(str1.isalpha())
+
+str1 = "hello world"
+print(str1.islower())
+
+str1 = "We wish you a Merry Christmas\n"
+print(str1.isprintable())
+str1 = "         "       #using Spacebar
+print(str1.isspace())
+str2 = "  "       #using Tab
+print(str2.isspace())
+
+str1 = "World Health Organization" 
+print(str1.istitle())
+
+str2 = "To kill a Mocking bird"
+print(str2.istitle())
+
+str1 = "Python is a Interpreted Language" 
+print(str1.startswith("Python"))
+
+str1 = "Python is a Interpreted Language" 
+print(str1.swapcase())
+
+str1 = "His name is Dan. Dan is an honest man."
+print(str1.title())
+
+#     Day 14
+
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+# Conditional operators 
+# >, <, >=, <=, ==, !=
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+if(a>18):
+  print("You can drive")
+  print("Yes")
+else:
+  print("You cannot drive")
+  print("No")
+  print("Yay!")
+
+applePrice = 10
+budget = 200
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
+
+num = int(input("Enter the value of num: "))
+if (num < 0):
+  print("Number is negative.")
+elif (num == 0):
+  print("Number is Zero.")
+elif (num == 999):
+  print("Number is Special.")
+else:
+  print("Number is positive.")
+
+print("I am happy now")
+
+num = 18
+if (num < 0):
+    print("Number is negative.")
+elif (num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num > 10 and num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
