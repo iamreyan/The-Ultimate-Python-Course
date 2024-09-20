9629a9c616920d396d496e49d68512849743cb61
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-09-20 19:08:49 +0500

    practise.py

--- practise.py
+++ practise.py
@@ -56,16 +56,6 @@ print(15//6)
 print(5%3)
 print(2**4)
 
-#     Day 8
-
-a = 50
-b = 3
-
-print("The value of", a, "+", 3, "is: ", a + b)
-print("The value of", a, "-", 3, "is: ", a - b)
-print("The value of", a, "*", 3, "is: ", a * b)
-print("The value of", a, "/", 3, "is: ", a / b)
-
 #     Day 9
 
 a = "1"
