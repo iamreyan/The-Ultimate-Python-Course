e67a18e80aa7cd4de0848e88a414bfae321ed2f7
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-09-27 18:08:46 +0500

    Update practise.py

--- practise.py
+++ practise.py
@@ -269,7 +269,26 @@ for k in range(1, 20001):
 for k in range(1, 12, 3):
   print(k)
 
+#     Day 18
 
+i = int(input("Enter the number: "))
+print(i)
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("Done with the loop")
+
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+else:
+  print("I am inside else")
+
+do {
+  loop body;
+}while(condition);
 
 
 
