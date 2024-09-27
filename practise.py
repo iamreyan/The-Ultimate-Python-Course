87310cb543d235dc62b89bb4483ae2d0bbd6d9ea
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-09-27 17:39:22 +0500

    Update practise.py

--- practise.py
+++ practise.py
@@ -234,10 +234,11 @@ match x:
     # if x is 0
     case 0:
         print("x is zero")
-    # case with if-condition
+    
     case 4:
         print("case is 4")
 
+    # cases with if-condition
     case _ if x!=90:
         print(x, "is not 90")
     case _ if x!=80:
