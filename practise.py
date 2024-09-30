ed7764aba2be1e2a93ea8c8281f807e755876c2a
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-09-30 20:02:40 +0500

    Update practise.py

--- practise.py
+++ practise.py
@@ -442,10 +442,58 @@ res = tuple1.index(3, 4, 8)
 res = len(tuple1)
 print('Count of 3 in tuple1 is:', res)
 
+#     Day 28
+
+letter = "Hey my name is {1} and I am from {0}"
+country = "India"
+name = "Harry"
+
+print(letter.format(country, name))
+print(f"Hey my name is {name} and I am from {country}")
+print(f"We use f-strings like this: Hey my name is {{name}} and I am from {{country}}")
+price = 49.09999
+txt = f"For only {price:.2f} dollars!"
+print(txt)
+# print(txt.format())
+print(type(f"{2 * 30}"))
+
+#     Day 29
+
+def square(n):
+  '''Takes in a number n, returns the square of n'''
+  print(n**2)
+square(5)
+print(square.__doc__)
+
+#     Day 30
+
+# factorial(7) = 7*6*5*4*3*2*1
+# factorial(6) = 6*5*4*3*2*1
+# factorial(5) = 5*4*3*2*1
+# factorial(4) = 4*3*2*1
+# factorial(0) = 1
 
 
+# factorial(n) = n * factorial(n-1)
+def factorial(n):
+  if (n == 0 or n == 1):
+    return 1
+  else:
+    return n * factorial(n - 1)
 
 
+print(factorial(5))
+# 5 * factorial(4)
+# 5 * 4 * factorial(3)
+# 5 * 4 * 3 * factorial(2)
+# 5 * 4 * 3 * 2 * factorial(1)
+# 5 * 4 * 3 * 2 * 1
 
+# Quick Quiz: Write a program to print the Fibonacci sequence
+# f(0) = 0
+# f(1) = 1
+# f(2) = f(1) + f(0)
+# f(n) = f(n-1) + f(n-2)
 
+# 0 1 1 2 3 5 8....
 
