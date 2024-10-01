778d154c4b0ec13849d78ce945d0b6c9d0db8f96
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-10-01 19:27:26 +0500

    Update practise.py

--- practise.py
+++ practise.py
@@ -497,3 +497,12 @@ print(factorial(5))
 
 # 0 1 1 2 3 5 8....
 
+"""
+def fibonacci(n):
+  if (n == 0):
+    return 0
+  elif(n == 1):
+    return 1
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+"""
