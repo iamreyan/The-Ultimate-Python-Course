9dc06c51ab0fb2369d349a97686523089a19692a
Reyan A. <153376900+iamreyan@users.noreply.github.com>
2024-10-05 16:39:52 +0500

    Update exersice.py

--- exersice.py
+++ exersice.py
@@ -23,3 +23,49 @@ elif hour >= 12 and hour < 17:
     print("Good Afternoon Sir", name,"!")
 else:
     print("Good Night Sir", name,"!")
+
+#     Exersice 3
+
+questoins = [
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4]
+    ["Which language used to make Facebook","Python","HTML","PHP","Javascript","None",4] 
+]
+
+levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
+money = 0
+
+for i in range(0, len(questoin)):
+    questoin = questoins[i]
+    print(f"\n\nQuestions for Rs. {levels[i]}")
+    print(f"a. {questoin[1]}          b. {questoin[2]}")
+    print(f"c. {questoin[3]}          d. {questoin[4]}")
+    reply = int(input("Enter your answer (1-4) or 0 to quit:\n"))
+    if(reply == 0):
+        money = levels[i-1]
+        break
+    if(reply == questoin[-1]):
+        print(f"Correct answer, you have won Rs.{levels[i]}")
+        if(i==4):
+            money = 10000 
+        elif (i == 9):
+            money = 320000
+        elif(i ==14):
+            money = 10000000
+    else:
+        print("Wrong answer!")
+        break 
+
+print("Your take home money is {money}")
+
+#     Exersice 4
+
+
